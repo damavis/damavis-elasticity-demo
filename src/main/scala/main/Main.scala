acfb211b14f99d3a4122ba28ba1ba2b57d79bfa8
Daniel Bestard <daniel.bestard@damavis.com>
2021-12-17 13:39:05 +0100

    make main as companion object

--- src/main/scala/main/Main.scala
+++ src/main/scala/main/Main.scala
@@ -1,9 +1,39 @@
 package main
 
 import com.damavis.elasticity.Arguments
-import main.pipeline.PipelineInstance
+import com.damavis.elasticity.stage.{ComputeMeanPrice, CreateLogFeatures, FitMultiplicativeDemandModel, PipelineStage}
+import com.damavis.elasticity.pipeline.Pipeline
+import com.damavis.elasticity.repository.resource.{CSVResourceImpl, FileResource}
+import com.damavis.elasticity.repository.{Repository, RepositoryImpl}
+import com.damavis.spark.{SparkApp, SparkConf}
+import com.typesafe.config.{Config, ConfigFactory}
+
+class Main(arguments: Arguments) extends SparkApp with SparkConf {
+
+  override val name: String = "demo-price-elasticity"
+
+  val config: Config = ConfigFactory.load(arguments.environment)
+
+  def run(): Unit = {
+    // Resources
+    val resource: FileResource = new CSVResourceImpl()
+
+    // Repository
+    val repository: Repository = new RepositoryImpl(config, resource)
+
+    // Interactors
+    val computeMeanPrice: PipelineStage = new ComputeMeanPrice()
+    val createLogFeatures: PipelineStage = new CreateLogFeatures()
+    val fitMultiplicativeDemandModel: PipelineStage = new FitMultiplicativeDemandModel()
+
+    // Pipeline
+    val pipeline = new Pipeline(repository, createLogFeatures, computeMeanPrice, fitMultiplicativeDemandModel)
+
+    pipeline.run()
+  }
+}
 
 object Main extends App {
   val arguments: Arguments = Arguments.getArguments(args).get
-  new PipelineInstance(arguments).run()
+  new Main(arguments).run()
 }
